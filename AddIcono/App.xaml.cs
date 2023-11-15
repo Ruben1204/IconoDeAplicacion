7121529f88c9c6227f4951cea2d0937bdfe77c61
Ruben Rueda <ingeniero.rubenrueda@gmail.com>
2023-11-15 16:37:38 -0500

    FirstCommit
    Add implementation IconApp with propertys

---
+++ AddIcono/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AddIcono
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AddIcono/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AddIcono
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
